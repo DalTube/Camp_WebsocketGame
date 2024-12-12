44c26bf94be85218e2c14f345e5bdd0d38367dae
EuiJoong <euijki87@gmail.com>
2024-12-12 14:43:58 +0900

    add 초기 파일

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ import express from 'express';
 import { createServer } from 'http';
 import initSocket from './init/socket.js';
 import { loadGameAssets } from './init/assets.js';
+import dotenv from 'dotenv';
+import redis from 'redis';
+
+dotenv.config(); // env환경변수 파일 가져오기
+
+/*** Redis 연동 */
+const client = redis.createClient();
 
 const app = express();
 const server = createServer(app);
-
 const PORT = 3000;
 
 app.use(express.json());
