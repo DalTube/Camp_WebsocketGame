12d49474f8e30c6a9d1c61081b09047c4cccbb5e
EuiJoong <euijki87@gmail.com>
2024-12-13 22:13:18 +0900

    update 스테이지 구분 및 점수 획득 기능 추가

--- public/js/game.js
+++ public/js/game.js
@@ -2,8 +2,42 @@ import Player from './models/Player.js';
 import Ground from './models/Ground.js';
 import CactiController from './controller/CactiController.js';
 import Score from './models/Score.js';
+import Stage from './models/Stage.js';
 import ItemController from './controller/ItemController.js';
-import { sendEvent } from './websocket/Socket.js';
+import { getGameAssets, sendEvent } from './websocket/Socket.js';
+
+const stageAsset = {
+  name: 'stage',
+  version: '1.0.0',
+  data: [
+    { id: 1000, score: 0, scorePerSecond: 1 },
+    { id: 1001, score: 5, scorePerSecond: 2 },
+    { id: 1002, score: 20, scorePerSecond: 4 },
+    { id: 1003, score: 50, scorePerSecond: 8 },
+  ],
+};
+
+const itemAsset = {
+  name: 'item',
+  version: '1.0.0',
+  data: [
+    { id: 1, score: 10, image: '/images/items/pokeball_red.png' },
+    { id: 2, score: 20, image: '/images/items/pokeball_yellow.png' },
+    { id: 3, score: 30, image: '/images/items/pokeball_purple.png' },
+    { id: 4, score: 40, image: '/images/items/pokeball_cyan.png' },
+  ],
+};
+
+const itemUnlockAsset = {
+  name: 'item_unlock',
+  version: '1.0.0',
+  data: [
+    { id: 101, stage_id: 1001, item_id: 1 },
+    { id: 201, stage_id: 1002, item_id: 2 },
+    { id: 301, stage_id: 1002, item_id: 3 },
+    { id: 401, stage_id: 1002, item_id: 4 },
+  ],
+};
 
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
@@ -40,12 +74,23 @@ const CACTI_CONFIG = [
 ];
 
 // 아이템
-const ITEM_CONFIG = [
-  { width: 50 / 1.5, height: 50 / 1.5, id: 1, image: '/images/items/pokeball_red.png' },
-  { width: 50 / 1.5, height: 50 / 1.5, id: 2, image: '/images/items/pokeball_yellow.png' },
-  { width: 50 / 1.5, height: 50 / 1.5, id: 3, image: '/images/items/pokeball_purple.png' },
-  { width: 50 / 1.5, height: 50 / 1.5, id: 4, image: '/images/items/pokeball_cyan.png' },
-];
+// const ITEM_CONFIG = [
+//   { width: 50 / 1.5, height: 50 / 1.5, id: 1, image: '/images/items/pokeball_red.png' },
+//   { width: 50 / 1.5, height: 50 / 1.5, id: 2, image: '/images/items/pokeball_yellow.png' },
+//   { width: 50 / 1.5, height: 50 / 1.5, id: 3, image: '/images/items/pokeball_purple.png' },
+//   { width: 50 / 1.5, height: 50 / 1.5, id: 4, image: '/images/items/pokeball_cyan.png' },
+// ];
+
+const ITEM_CONFIG = [];
+itemAsset.data.forEach((item) => {
+  ITEM_CONFIG.push({
+    width: 50 / 1.5,
+    height: 50 / 1.5,
+    id: item.id,
+    image: item.image,
+  });
+});
+
 // const ITEM_CONFIG = [
 //   { width: 50 / 1.5, height: 50 / 1.5, id: 1, image: 'images/items/pokeball_red.png' },
 //   { width: 50 / 1.5, height: 50 / 1.5, id: 2, image: 'images/items/pokeball_yellow.png' },
@@ -66,6 +111,11 @@ let gameSpeed = GAME_SPEED_START;
 let gameover = false;
 let hasAddedEventListenersForRestart = false;
 let waitingToStart = true;
+
+// ------- 추가 --------------
+let stage = null;
+const MAX_GAME_STAGE = stageAsset.data.length;
+console.log('MAX_GAME_STAGE : ', MAX_GAME_STAGE);
 
 function createSprites() {
   // 비율에 맞는 크기
@@ -116,6 +166,8 @@ function createSprites() {
   itemController = new ItemController(ctx, itemImages, scaleRatio, GROUND_SPEED);
 
   score = new Score(ctx, scaleRatio);
+  //stage객체 생성시 첫 스테이지 id와 다음 스테이지 id 데이터 넘김
+  stage = new Stage(ctx, scaleRatio);
 }
 
 function getScaleRatio() {
@@ -174,7 +226,9 @@ function reset() {
   ground.reset();
   cactiController.reset();
   score.reset();
+  stage.reset();
   gameSpeed = GAME_SPEED_START;
+
   // 게임시작 핸들러ID 2, payload 에는 게임 시작 시간
   sendEvent(2, { timestamp: Date.now() });
 }
@@ -202,12 +256,15 @@ function gameLoop(currentTime) {
   }
 
   // 모든 환경에서 같은 게임 속도를 유지하기 위해 구하는 값
-  // 프레임 렌더링 속도
+  // 프레임 렌더링 속도 (프레임당 걸리는 시간)
   const deltaTime = currentTime - previousTime;
   previousTime = currentTime;
 
   clearScreen();
 
+  /***
+   * 게임 시작 후 진행 상태 처리 (랜더링, 값)
+   */
   if (!gameover && !waitingToStart) {
     // update
     // 땅이 움직임
@@ -219,9 +276,22 @@ function gameLoop(currentTime) {
     player.update(gameSpeed, deltaTime);
     updateGameSpeed(deltaTime);
 
-    score.update(deltaTime);
+    let currnetStage = stage.getStage();
+    const isStageChange = score.update(
+      deltaTime,
+      stageAsset.data[currnetStage - 1],
+      currnetStage < MAX_GAME_STAGE ? stageAsset.data[currnetStage] : null,
+    );
+
+    if (isStageChange && currnetStage < MAX_GAME_STAGE) {
+      stage.update(stage.getStage() + 1);
+    }
+    // stage.update();
   }
 
+  /***
+   * 게임 오버가 발생 했을 때 처리 (사물과 충돌)
+   */
   if (!gameover && cactiController.collideWith(player)) {
     gameover = true;
     score.setHighScore();
@@ -230,6 +300,10 @@ function gameLoop(currentTime) {
     // 게임종료 핸들러ID 3, payload 에는 게임 시작 시간
     sendEvent(3, { timestamp: Date.now() });
   }
+
+  /***
+   * 아이템 획득 시 처리
+   */
   const collideWithItem = itemController.collideWith(player);
   if (collideWithItem && collideWithItem.itemId) {
     score.getItem(collideWithItem.itemId);
@@ -241,11 +315,18 @@ function gameLoop(currentTime) {
   ground.draw();
   score.draw();
   itemController.draw();
+  stage.draw();
 
+  /***
+   * 게임오버 상태 이후 처리
+   */
   if (gameover) {
     showGameOver();
   }
 
+  /***
+   * 게임 시작 전 대기 랜더링 처리
+   */
   if (waitingToStart) {
     showStartGameText();
   }
@@ -254,7 +335,12 @@ function gameLoop(currentTime) {
   requestAnimationFrame(gameLoop);
 }
 
+/***
+ * 게임 테이블 데이터를 가져오고 난 후 게임 실행을 위해 setTimeout 사용
+ */
 // 게임 프레임을 다시 그리는 메서드
+// setTimeout(() => {
 requestAnimationFrame(gameLoop);
+// }, 500);
 
 window.addEventListener('keyup', reset, { once: true });
