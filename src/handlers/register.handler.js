699c90da823c394aa529a9a6165800a65ac00934
EuiJoong <euijki87@gmail.com>
2024-12-13 11:24:07 +0900

    fix 콘솔로그 주석처리

--- src/handlers/register.handler.js
+++ src/handlers/register.handler.js
@@ -3,23 +3,22 @@ import { v4 as uuidv4 } from 'uuid';
 import { handleDisconnect, handlerEvent, handleConnection } from './helper.js';
 
 const registerHandler = (io) => {
-   io.on('connection', (socket) => {
-      //이벤트 처리
-      const userUUID = uuidv4();
+  io.on('connection', (socket) => {
+    //이벤트 처리
+    const userUUID = uuidv4();
 
-      addUser({ uuid: userUUID, socketId: socket.id });
-      console.log('userUUID : ', userUUID, ' socket.id : ', socket.id);
+    addUser({ uuid: userUUID, socketId: socket.id });
 
-      handleConnection(socket, userUUID);
+    handleConnection(socket, userUUID);
 
-      //이벤트 소켓
-      socket.on('event', (data) => handlerEvent(io, socket, data));
+    //이벤트 소켓
+    socket.on('event', (data) => handlerEvent(io, socket, data));
 
-      //접속 해제시 이벤트
-      socket.on('disconnect', (socket) => {
-         handleDisconnect(socket, userUUID);
-      });
-   });
+    //접속 해제시 이벤트
+    socket.on('disconnect', (socket) => {
+      handleDisconnect(socket, userUUID);
+    });
+  });
 };
 
 //npm install uuid
