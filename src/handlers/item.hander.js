1892343698d58559c634a8ef60431615b0e50bbd
EuiJoong <euijki87@gmail.com>
2024-12-17 16:38:25 +0900

    update 서버 아이템 생성 검증 구현

--- src/handlers/item.hander.js
+++ src/handlers/item.hander.js
@@ -1,24 +1,47 @@
 import { getGameAssets } from '../init/assets.js';
 import { setItem } from '../models/item.model.js';
 
+/***
+ * 아이템을 획득 했을 때 유효한 아이템인지 검증 처리
+ */
 export const getItemHandler = (uuid, payload) => {
   // 1. 데이터 테이블에서 아이템, 아이템 해제 정보 가져오기
   const { items, itemUnlocks } = getGameAssets();
 
   // 2. 가져온 데이터 테이블의 데이터 존재여부 체크
   if (!items) {
-    return { stauts: 'fail', message: 'No items data table' };
+    return { stauts: 'fail', message: 'This item does not exist.' };
   }
 
   if (!itemUnlocks) {
-    return { stauts: 'fail', message: 'No itemunlocks data table' };
+    return { stauts: 'fail', message: 'This itemunlocks does not exist.' };
   }
 
   // 3. 유효한 아이템인지 검증
-  if (!items.data.some((item) => item.id === payload.id)) {
+  if (!items.data.some((item) => item.id === payload.itemId)) {
     return { status: 'fail', message: 'Item not found' };
   }
 
   // 4. 아이템 정보 저장
   setItem(uuid, id);
+};
+
+/***
+ * 아이템이 생성되었을 때 정상적인 아이템이 생성된 것인지 확인
+ */
+export const createItemHandler = (payload) => {
+  // 1. 데이터 테이블에서 아이템, 아이템 해제 정보 가져오기
+  const { items, itemUnlocks } = getGameAssets();
+
+  // 2. 아이템 데이터 검증
+  if (!items.data.some((item) => item.id === payload.itemId)) {
+    return { status: 'fail', message: 'This item does not exist.' };
+  }
+
+  // 3. 아이템 해금 데이터 검증
+  itemUnlocks.data.forEach((itemUnlock) => {
+    if (itemUnlock.item_id === payload.itemId && itemUnlock.stage_id > payload.stageId) {
+      return { status: 'fail', message: 'The correct item was not created.' };
+    }
+  });
 };
