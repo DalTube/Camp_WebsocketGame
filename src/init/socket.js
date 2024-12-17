a976b3adb1392c2d013246dde313e96026c7c517
EuiJoong <euijki87@gmail.com>
2024-12-17 15:38:08 +0900

    fix 수정

--- src/init/socket.js
+++ src/init/socket.js
@@ -3,8 +3,8 @@ import registerHandler from '../handlers/register.handler.js';
 //SocketIo셋팅
 const initSocket = (server) => {
   const io = new SocketIO();
-  io.attach(server); //서버를 소켓 IO와 연결
-
+  //서버를 소켓 IO와 연결
+  io.attach(server);
   //핸들러
   registerHandler(io);
 };
