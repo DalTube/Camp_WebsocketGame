a8833b799d73fe310ecef3fe72287c673a9efc5d
EuiJoong <euijki87@gmail.com>
2024-12-17 15:35:57 +0900

    update 아이템 핸들러 추가

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,9 +1,11 @@
 import { gameEnd, gameStart } from './game.handler.js';
+import { getItemHandler } from './item.hander.js';
 import { moveStageHandler } from './stage.handler.js';
 
 const handlerMappings = {
   2: gameStart,
   3: gameEnd,
+  4: getItemHandler,
   11: moveStageHandler,
 };
 
