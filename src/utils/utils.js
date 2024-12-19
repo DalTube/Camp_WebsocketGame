49ce406bceacd4c1c5bb9c6c074d9b76bce3e92e
EuiJoong <euijki87@gmail.com>
2024-12-19 10:22:41 +0900

    delete 파일 삭제

--- src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-/** basePath */
-export const getBasePath = (metaUrl) => {
-  const __filename = fileURLToPath(metaUrl);
-  const __dirname = path.dirname(__filename);
-  return path.join(__dirname, '../../../public');
-};
